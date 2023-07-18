914a3f4bd7f6d7beb034a7eda7f0760d30c7a530
Majid Ahmadi Jebeli <itpaz@outlook.com>
2023-07-18 10:50:33 +0330

    feature(swagger): Add Swagger configuration in the main class

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,30 @@
 import { NestFactory } from '@nestjs/core'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  await app.listen(3000)
+
+  /** Swagger */
+  const config = new DocumentBuilder()
+    .setTitle('Base Project')
+    .setDescription('Base project blueprint')
+    .setVersion(require('../package.json').version)
+    .build()
+  const document = SwaggerModule.createDocument(app, config)
+  document.components = {
+    ...document.components,
+    securitySchemes: {
+      ApiAccessToken: {
+        type: 'apiKey',
+        in: 'header',
+        name: 'API_ACCESS_TOKEN',
+      },
+    },
+  }
+  SwaggerModule.setup('api-doc', app, document)
+
+  await app.listen(3000).then(async () => console.info('Service is running %s/api-doc', await app.getUrl()))
 }
 bootstrap()
