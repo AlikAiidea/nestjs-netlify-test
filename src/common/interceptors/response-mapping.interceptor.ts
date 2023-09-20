5c4fda5e5094452a18b3f53bf2836d8cbd0b79b7
Matin Askari <dev.mtnaskari@gmail.com>
2023-09-20 23:55:28 +0330

    chore(env): Add response mapping interceptor

---
+++ src/common/interceptors/response-mapping.interceptor.ts
@@ -0,0 +1,32 @@
+import { Observable } from 'rxjs'
+import { catchError, map } from 'rxjs/operators'
+
+import { CallHandler, ExecutionContext, HttpException, Injectable, NestInterceptor } from '@nestjs/common'
+
+import { IResponse } from '../dto/interfaces/response.interface'
+
+/**
+ * @description: Interceptor for response integration.
+ * @param T data the data to be returned if the request is successful.
+ * @param boolean success status of the request.
+ * @param string message message to be returned if the request is not successful.
+ * @returns {IResponse<T>}
+ * @example: @UseInterceptors(ResponseMappingInterceptor)
+ */
+@Injectable()
+export class ResponseMappingInterceptor<T> implements NestInterceptor<T, IResponse<T>> {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<IResponse<T>> {
+    return next.handle().pipe(
+      map((data) => data),
+      catchError(async (error) => {
+        throw new HttpException(
+          {
+            success: false,
+            message: error.response ? error.response.message ?? 'Unknown error' : 'Unknown error',
+          },
+          error.response ? error.response.statusCode : 500,
+        )
+      }),
+    )
+  }
+}
