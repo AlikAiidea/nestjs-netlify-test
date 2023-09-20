31b3b602b1b1afed1b7bf3b4f9e7c1fb03b493e0
Matin Askari <dev.mtnaskari@gmail.com>
2023-09-20 23:48:08 +0330

    chore(env): Update app controller test

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,23 +1,28 @@
 import { Test, TestingModule } from '@nestjs/testing'
 
 import { AppController } from './app.controller'
-import { AppService } from './app.service'
 
 describe('AppController', () => {
-  let appController: AppController
+  let controller: AppController
 
   beforeEach(async () => {
-    const app: TestingModule = await Test.createTestingModule({
+    const module: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
-      providers: [AppService],
+      providers: [],
     }).compile()
 
-    appController = app.get<AppController>(AppController)
+    controller = module.get<AppController>(AppController)
   })
 
-  describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!')
+  it('should be defined', () => {
+    expect(controller).toBeDefined()
+  })
+
+  describe('showVersion', () => {
+    it('should return the project version', () => {
+      const expectedVersion = require('../package.json').version
+      const result = controller.showVersion()
+      expect(result).toEqual({ version: expectedVersion })
     })
   })
 })
